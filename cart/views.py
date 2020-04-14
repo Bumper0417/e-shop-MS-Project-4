ecd2b16ad63d69631f8214322cb9b31b1a18370f
Bumper0417 <chrisgoros13@gmail.com>
2020-04-14 12:54:54 +0000

    Added margin in my registration form

--- cart/views.py
+++ cart/views.py
@@ -22,7 +22,7 @@ def add_to_cart(request, id):
         cart[id] = cart.get(id, quantity) 
 
     request.session['cart'] = cart
-    return redirect(reverse('index'))
+    return redirect(reverse('items'))
     
 
 def adjust_cart(request, id):
