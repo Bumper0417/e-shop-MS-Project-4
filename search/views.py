30680cee2684cf1d576cb46200f63c38a9869c76
Bumper0417 <chrisgoros13@gmail.com>
2020-04-14 19:58:13 +0000

    Testing my search functionality

--- search/views.py
+++ search/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
 from items.models import Item
 
 # Create your views here.
 
 
 def do_search(request):
+    
     items = Item.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "items.html", {"items": items})
+    if 'q' in request.GET:
+        return render(request, "items.html", {"items": items})
+    elif 'q' == None:
+        return Http404('Enterr your search criteria')
+    else:
+        return Http404('Item not found!')
+    
