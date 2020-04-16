bfd515606b7125e989b0f045d415b034d6fda23b
Bumper0417 <chrisgoros13@gmail.com>
2020-04-16 16:07:51 +0000

    Testing the reset form

--- eshop/settings.py
+++ eshop/settings.py
@@ -172,8 +172,8 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-EMAIL_PORT = 587
+EMAIL_PORT = 465
