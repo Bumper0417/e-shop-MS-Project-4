e4be085608e5822a5573767f166c86e5720bd2d4
Bumper0417 <chrisgoros13@gmail.com>
2020-04-17 14:00:16 +0000

    Set Debug = False in my settings.py file

--- eshop/settings.py
+++ eshop/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #Set to false before deployment
+DEBUG = False #Set to false before deployment
 
 ALLOWED_HOSTS = ['e-shop-ms-project4.herokuapp.com', 'localhost']
 
